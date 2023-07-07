739725537b6604588705f16f87a90d452cd54562
kwarnerredhat <kwarner@redhat.com>
2023-07-07 12:07:31 -0400

    accessed objects

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -3,8 +3,18 @@ const shoes = {
     size: 12,
     Fashion: "Designer",
     saySize: function() {
+        
         console.log("The shoe size is" + this.size);
+        
     }
 };
 shoes.saySize();
+const type = new Object();
+type.name = 'Rick Owens';
+type.Fashion = 'Designer';
+type.size = 12;
+
+console.log(type.name); //output is Rick Owens
+console.log(type['fashion']);//output is Designer
+
 
