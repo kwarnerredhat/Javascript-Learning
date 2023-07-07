1f7042743b87ee7af3726df8c335f73b96e5d46d
kwarnerredhat <kwarner@redhat.com>
2023-07-07 14:22:01 -0400

    added a class

---
+++ Lesson3/shoes.js
@@ -0,0 +1,11 @@
+class shoes {
+    constructor(type, size, Fashion) {
+        this.type = type;
+        this.size = size;
+        this.Fashion = Fashion;
+    }
+
+    sayFashion() {
+        console.log('Fashion, ' + this.Fashion + '!');
+    }
+}
