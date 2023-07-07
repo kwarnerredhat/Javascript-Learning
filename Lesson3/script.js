067d91c0ac7abfe53ed417bccdf026e62987eb03
kwarnerredhat <kwarner@redhat.com>
2023-07-07 10:47:39 -0400

    Added some js

--- Lesson3/script.js
+++ Lesson3/script.js
@@ -1,7 +1,13 @@
+alert('test')
 const shoes = {
     name:"Rick Owens",
     size:"11",
-    color:"Black",
-};
+    color:"Black"
+    ,
+}; 
+<p id="demo"></p>
 
-
+document.getElementById("demo").innerHTML = shoes.name + "are" + shoes.size + "is 12.";
+Fullname : functio() {
+    return this.shoesname + " " + this.shoessize;
+}
