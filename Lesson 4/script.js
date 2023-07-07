7f934c28ee1c82a09c30dff4f4da15019dbf04c5
kwarnerredhat <kwarner@redhat.com>
2023-07-07 14:34:27 -0400

    mix text with variables

---
+++ Lesson 4/script.js
@@ -0,0 +1,23 @@
+const shoes = {
+    type: "Rick Owens",
+    size: 12,
+    Fashion: "Designer",
+    saySize: function() {
+        
+        console.log("The shoe size is" + this.size);
+        console.log("The shoe type are" + this.type);
+    }
+};
+shoes.saySize();
+const type = new Object();
+type.name = 'Rick Owens';
+type.Fashion = 'Designer';
+type.size = 12;
+
+console.log(type.name); 
+
+var query = "fashion";
+
+console.log(type['fashion']);
+
+
