f148c825b9acf3793d7e572435a7b26f8c3b3e19
kwarnerredhat <kwarner@redhat.com>
2023-07-10 13:46:23 -0400

    some variables

---
+++ Lesson 6/script.js
@@ -0,0 +1,17 @@
+var color = "teal";
+
+document.querySelector(".left").style.backgroundColor = color;
+document.querySelector("left .color-value").innerHTML = color;
+
+color = "skyblue"
+
+function headingColor() {
+   let color = "red";
+    document.querySelector(".title").style.color = color;
+}
+
+headingColor();
+
+document.querySelector(".right").style.backgroundColor = color;
+document.querySelector("right .color-value").innerHTML = color;
+
