d0c55596841458219eec706adc362997e4f54a65
kwarnerredhat <kwarner@redhat.com>
2023-07-19 12:57:26 -0400

    Practice: Build a new object with a constructor

---
+++ Lesson3/Practice: Build a new object with a constructor/script.js
@@ -0,0 +1,31 @@
+const person1 = {
+    name: "Khalil",
+    age: "22",
+    gender: "Male",
+};
+
+const person2 = {
+    name: "Aminah",
+    age: "44",
+    gender: "Female",
+};
+
+const address = {
+    street: "100 E Davie St",
+    city: "Raleigh",
+    zipcode: "27606",
+};
+
+const person3 = {
+    name: "Emily",
+    age: "34",
+    gender: "Female",
+    address: address,
+};
+constructor(message) {
+    this.name = "Khalil";
+}
+
+console.log("Person 1:", person1);
+console.log("Person 2:", person2);
+console.log("Person 3:", person3);
