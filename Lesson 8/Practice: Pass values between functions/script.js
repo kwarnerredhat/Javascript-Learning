12cf49bc261dd6588db690b703dc73cc2e061964
kwarnerredhat <kwarner@redhat.com>
2023-07-24 12:27:49 -0400

    new passed value function

--- Lesson 8/Practice: Pass values between functions/script.js
+++ Lesson 8/Practice: Pass values between functions/script.js
@@ -1,18 +1,8 @@
-const object = {
-    name: "Khalil Warner",
-    age: 22,
-    role: "Software Engineer",
-};
-function displayName() {
-    const outputElement = document.querySelector("output");
-    outputElement.append(object.name);
-    displayAge();
-  }
-
-  function displayAge() {
-    const outputElement = document.querySelector("output");
-    outputElement.append(`Age: ${object.age}`);
-  }
-  
-  displayName();
-  
+function khalilFunction(number1, number2) {
+  let result = number1 + number2;
+  return result;
+}
+let num1 = 11
+let num2 = 11
+let sum = khalilFunction(num1, num2);
+console.log("My age" + "currently" + "is" + sum);
