d0acc377fd992de77ba038cea9d4e742dbc91c2b
kwarnerredhat <kwarner@redhat.com>
2023-07-24 11:53:13 -0400

    My new function

--- Lesson 8/Practice: Build a function/script.js
+++ Lesson 8/Practice: Build a function/script.js
@@ -1,5 +1,14 @@
-function greet(name) {
-    console.log("Hello, " + name + "!");
+function KhalilFunction(sentence) {
+  const words = sentence.split(" ");
+ const capitalizedWords = words.map(word => {
+   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+  });
+const capitalizedSentence = capitalizedWords.join(" ");
+return capitalizedSentence;
 }
+const inputSentence = "Hey this is Khalil the fashion connoisseur.";
+const capitalizedSentence = KhalilFunction(inputSentence);
+console.log(capitalizedSentence);
+ 
 
-greet("Khalil Warner");
+
