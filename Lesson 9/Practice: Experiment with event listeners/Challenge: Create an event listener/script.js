61d4df2b34eb788bd62b044de54f4f6fd3e04217
kwarnerredhat <kwarner@redhat.com>
2023-07-24 14:17:24 -0400

    My new event listener challenge

--- Lesson 9/Practice: Experiment with event listeners/Challenge: Create an event listener/script.js
+++ Lesson 9/Practice: Experiment with event listeners/Challenge: Create an event listener/script.js
@@ -1,5 +1,8 @@
-function handleClick() {
-  alert("Button clicked");
-}
-const myButton = doucment.querySelector("Button");
-myButton.addEventListener("click", handleClick);
+document.addEventListener("DOMContentLoaded", function() {
+  const button = document.getElementById("khalilButton");
+  const messageParagraph = document.getElementById("khalilMessage");
+  function khalilEventListener() { 
+ messageParagraph.textContent = "When it comes to fashion it is endless.";
+  }
+  button.addEventListener("click", khalilEventListener);
+});
